d7cda2982f4bb6faf1014826810eef651868c46e
Sayma-Shimu <ahmedsayma399@gmail.com>
2025-07-30 22:41:18 +0600

    nested if-else condition added

---
+++ nested.js
@@ -0,0 +1,22 @@
+/*nestted if-else condition-------------*/
+const money = 0;
+if (money > 300) {
+    console.log('bro u r rich');
+
+}
+else {
+    if (money > 100) {
+        console.log('tui rich na abr fokira oo na');
+
+    }
+    else {
+        if (money > 0) {
+            console.log('dosto tui kola kha calcium bara');
+
+        }
+        else {
+            console.log('tui amr frnd na');
+
+        }
+    }
+}
